3c891f9eb11d3aef8fb3ae7deb10525799b7b8ee
ocruz-utslp <ocruz-utslp@plataforma-utslp.net>
2024-02-23 18:30:59 -0600

    23.02.2024-2

--- log.sh
+++ log.sh
@@ -2,9 +2,13 @@
 file=cpu.log
 
 cd /root/
-if !(find $file)
-then
-   touch $file
-fi
-echo -e "$(date +%d/%m/%Y-%H:%M:%S)\t$(hostname -A)\t$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')\t$(free -m -t | grep T | awk '{usage=($3*100)/$2} END {print usage "%"}')\t$(df --total | grep total | awk '{print $5}')" >> $file
+while true; do
 
+	if !(find $file)
+	then
+	   touch $file
+	fi
+	echo -e "$(date +%d/%m/%Y-%H:%M:%S)\t$(hostname -A)\t$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')\t$(free -m -t | grep T | awk '{usage=($3*100)/$2} END {print usage "%"}')\t$(df --total | grep total | awk '{print $5}')" >> $file
+	sleep 5
+
+done
